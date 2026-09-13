9a7a51630d556b813b836d913a3088ca6bdb0768
Vladimir Atapin <atapin@gmail.com>
2026-09-13 22:42:55 +0200

    Correct dispatchable trait namespace

--- platform/app/Jobs/PreparePublicVideo.php
+++ platform/app/Jobs/PreparePublicVideo.php
@@ -4,10 +4,10 @@ namespace App\Jobs;
 
 use App\Models\Media;
 use App\Services\PublicVideoOptimizer;
-use Illuminate\Bus\Dispatchable;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
 class PreparePublicVideo implements ShouldQueue, ShouldBeUnique
